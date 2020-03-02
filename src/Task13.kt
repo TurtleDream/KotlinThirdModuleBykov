0c55c4647fc5ea69ee4707abe5d1f197b90e8915
TurtleDream <stanislav.byckov@gmail.com>
2020-03-02 11:40:57 +0300

    Add Task13

---
+++ src/Task13.kt
@@ -0,0 +1,11 @@
+// Задание 12: Количество сотрудников
+// Напишите функцию, принимающую на вход имена сотрудников и возвращающую их количество. Используйте для этого vararg, из предыдущей темы.
+fun main(){
+    val employerNames = arrayOf("Геркулес", "Эдип", "Герадот", "Гепатит", "Дедал")
+
+    println("Количество сотрудников: ${calculateEmployersCount(*employerNames)}")
+}
+
+fun calculateEmployersCount (vararg employerNames: String): Int {
+    return employerNames.count()
+}
